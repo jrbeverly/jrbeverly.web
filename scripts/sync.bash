567b46185897719aa6d1fb41d1ccf42acb182653
jrbeverly <jonathan@jrbeverly.me>
2021-10-21 22:23:37 -0500

    Fix syncing issues

--- scripts/sync.bash
+++ scripts/sync.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-DIR_OUTPUT="${DIR}/outputs"
+DIR_ROOT="$( dirname "${DIR}" )"
+DIR_OUTPUT="${DIR_ROOT}/outputs"
 
 mkdir -p "${DIR_OUTPUT}"
 
@@ -11,43 +12,48 @@ dir_repo="${DIR}/repositories"
 mkdir -p "${dir_repo}"
 while IFS= read -r org; do
     echo "[GitHub] Synchronizing from ${org}"
-    # get_repos_in_organization "${dir_repo}/${org}" "${org}"
+    get_repos_in_organization "${dir_repo}/${org}" "${org}"
 done <"${DIR}/data/organizations"
 
 user="$(cat "${DIR}/data/user")"
 echo "[GitHub] Synchronizing from ${user}"
-# get_repos_in_user "${dir_repo}/${user}" "${user}"
+get_repos_in_user "${dir_repo}/${user}" "${user}"
 
 echo "[GitHub] Synchronizing posts from Git Users"
-while IFS= read -r repo; do
-    echo "[GitHub/${repo}] Synchronizing properties"
-    resp="$(mktemp -t github-XXXXXXXXXXXXXXXXX.json)"
-    get_github_repo "${repo}" "${resp}"
+for dataset in "${DIR}/repositories"/* ; do
+    dataset_name="$(basename "${dataset}")"
+    while IFS= read -r repo; do
+        echo "[GitHub/${repo}] Synchronizing properties"
+        resp="$(mktemp -t github-XXXXXXXXXXXXXXXXX.json)"
+        get_github_repo "${repo}" "${resp}"
 
-    topics="$(jq -r '.topics' "${resp}")"
-    name="$(jq -r '.name' "${resp}")"
-    ssh_url="$(jq -r '.ssh_url' "${resp}")"
-    updated_at="$(jq -r '.updated_at' "${resp}")"
-    html_url="$(jq -r '.html_url' "${resp}")"
-    description="$(jq -r '.description' "${resp}")"
+        topics="$(jq -r '.topics' "${resp}")"
+        name="$(jq -r '.name' "${resp}")"
+        ssh_url="$(jq -r '.ssh_url' "${resp}")"
+        created_at="$(jq -r '.created_at' "${resp}")"
+        html_url="$(jq -r '.html_url' "${resp}")"
+        description="$(jq -r '.description' "${resp}")"
 
-    readme_resp="$(mktemp -t github-XXXXXXXXXXXXXXXXX.json)"
-    readme_file="$(mktemp -t github-XXXXXXXXXXXXXXXXX.md)"
-    get_github_repo_file "${repo}" "${readme_resp}"
-    
-    download_url="$(jq -r '.download_url' "${readme_resp}")"
-    curl -sSL "${download_url}" -o "${readme_file}"
+        readme_resp="$(mktemp -t github-XXXXXXXXXXXXXXXXX.json)"
+        readme_file="$(mktemp -t github-XXXXXXXXXXXXXXXXX.md)"
+        get_github_repo_file "${repo}" "${readme_resp}"
+        
+        download_url="$(jq -r '.download_url' "${readme_resp}")"
+        if [[ "${download_url}" == "" ]]; then
+            continue
+        fi
+        curl -sSL "${download_url}" -o "${readme_file}"
 
-    result_file="${DIR_OUTPUT}/${user}-${name}.md"
-    touch "${result_file}"
-    echo """+++
-author=\"jrbeverly\"
-title=\"${name}\"
-date=\"${updated_at}\"
-description=\"${description}\"
-tags=${topics}
-+++
-""" > "${result_file}" 
-    cat "${readme_file}" >> "${result_file}" 
-    exit 
-done <"${DIR}/repositories/jrbeverly"
+        result_file="${DIR_OUTPUT}/${dataset_name}-${name}.md"
+        touch "${result_file}"
+        echo """+++
+    author=\"jrbeverly\"
+    title=\"${name}\"
+    date=\"${created_at}\"
+    description=\"${description}\"
+    tags=${topics}
+    +++
+    """ > "${result_file}" 
+        cat "${readme_file}" >> "${result_file}" 
+    done <"${dataset}"
+done
