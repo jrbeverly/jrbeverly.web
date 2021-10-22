e2488283feaae1ac6fcc820947d99a4aac855356
jrbeverly <jonathan@jrbeverly.me>
2021-10-21 21:39:03 -0500

    Synchronizing projects

--- scripts/sync.bash
+++ scripts/sync.bash
@@ -6,6 +6,6 @@ source "${DIR}/github.bash"
 dir_repo="${DIR}/repositories"
 mkdir -p "${dir_repo}"
 while IFS= read -r org; do
-    echo "[GitHub] Synchronizing from $org"
+    echo "[GitHub] Synchronizing from ${org}"
     get_repos_in_organization "${dir_repo}/${org}" "${org}"
-done < "${DIR}/data/organizations"
+done <"${DIR}/data/organizations"
